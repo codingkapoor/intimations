fe094867571c42d90da1524f390bbebbe9918245
codingkapoor <mailtoshivamk@gmail.com>
2019-10-29 23:28:59 +0530

    styled leaves status screen

--- src/screens/LeavesStatusScreen.js
+++ src/screens/LeavesStatusScreen.js
@@ -2,9 +2,35 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 const LeavesStatusScreen = () => {
-    return <View>
-        <Text>Hello Again!</Text>
-    </View>;
-}
+    return (
+        <View style={styles.parent}>
+            <View style={styles.container}>
+                <Text style={styles.figure}>23</Text>
+                <Text style={styles.label}>Earned Leaves</Text>
+            </View>
+            <View style={styles.container}>
+                <Text style={styles.figure}>11</Text>
+                <Text style={styles.label}>Sick Leaves</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    parent: {
+        flex: 1,
+        justifyContent: 'space-around'
+
+    },
+    container: {
+        alignItems: 'center'
+    },
+    figure: {
+        fontSize: 110
+    },
+    label: {
+        fontSize: 24
+    }
+});
 
 export default LeavesStatusScreen;
