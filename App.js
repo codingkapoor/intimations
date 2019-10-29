9c07ebce329ffb7d0f8f28fd175de12b84b4f7cd
codingkapoor <mailtoshivamk@gmail.com>
2019-10-29 23:00:30 +0530

    init screen

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import LeavesStatusScreen from './src/screens/LeavesStatusScreen';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const AppNavigator = createStackNavigator({
+  Leaves: LeavesStatusScreen
 });
+
+const App = createAppContainer(AppNavigator);
+
+export default App;
---
+++ src/screens/LeavesStatusScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const LeavesStatusScreen = () => {
+    return <View>
+        <Text>Hello Again!</Text>
+    </View>;
+}
+
+export default LeavesStatusScreen;
