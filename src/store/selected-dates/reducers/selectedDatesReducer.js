a8c4c8d751d9b56bfa8ce12669cb8f19004966b7
codingkapoor <mailtoshivamk@gmail.com>
2019-11-13 08:30:46 +0530

    html color codes added

--- src/store/selected-dates/reducers/selectedDatesReducer.js
+++ src/store/selected-dates/reducers/selectedDatesReducer.js
@@ -1,14 +1,14 @@
 import { UPDATE_SELECTED_DATES } from '../actions/types';
 import _ from 'lodash';
 
-const colors = ['red', 'blue', 'yellow'];
+const colors = ['#D6EA38', '#9DAB27', '#555D17', '#FCA5A5', '#E36969', '#9B0707'];
 
 const selectedDatesReducer = (selectedDates = {}, { type, payload }) => {
     if (type === UPDATE_SELECTED_DATES) {
         if (selectedDates[payload]) {
             let counter = selectedDates[payload].counter
-            
-            if (counter === 2)
+
+            if (counter === 5)
                 return _.omit(selectedDates, payload);
             else {
                 newCounter = counter + 1;
@@ -19,7 +19,7 @@ const selectedDatesReducer = (selectedDates = {}, { type, payload }) => {
                 return { [payload]: value, ..._.omit(selectedDates, payload) };
             }
         } else
-            return { [payload]: { selected: true, selectedColor: 'red', counter: 0 }, ...selectedDates };
+            return { [payload]: { selected: true, selectedColor: `${colors[0]}`, counter: 0 }, ...selectedDates };
     }
 
     return selectedDates;
