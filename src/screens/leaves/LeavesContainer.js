1baa22e7a18424d0b673d9ca448c871b724d12f6
codingkapoor <mailtoshivamk@gmail.com>
2019-11-21 22:21:43 +0530

    wire fetchemployee to leaves

--- src/screens/leaves/LeavesContainer.js
+++ src/screens/leaves/LeavesContainer.js
@@ -1,8 +1,9 @@
 import { connect } from 'react-redux';
 import LeavesScreen from './LeavesScreen';
+import { fetchEmployeeDetails } from '../../store/employee-details/actions';
 
 const mapStateToProps = ({ employeeDetails, pullToRefresh }) => {
     return { employeeDetails, pullToRefresh };
 };
 
-export default connect(mapStateToProps)(LeavesScreen);
+export default connect(mapStateToProps, { fetchEmployeeDetails })(LeavesScreen);
