c474eb19221ab3e2795ebc38f2a69b2d4d4fffd8
codingkapoor <mailtoshivamk@gmail.com>
2019-11-24 18:58:20 +0530

    remodeled holiday

--- src/common/utils/dates.js
+++ src/common/utils/dates.js
@@ -1,5 +1,7 @@
 export const MONTH_NAMES = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
+export const WEEK_NAMES = ["SUN", "MON", "TUE", "WED", "THUR", "FRI", "SAT"];
+
 export const getOrdinal = n => {
     return (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
 }
