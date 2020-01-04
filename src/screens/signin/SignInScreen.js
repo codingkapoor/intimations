00dba6762b97224219b6a039c86ea4d4e92b4dd6
codingkapoor <mailtoshivamk@gmail.com>
2020-01-05 01:02:20 +0530

    basic components

--- src/screens/signin/SignInScreen.js
+++ src/screens/signin/SignInScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TextInput, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const SignInScreen = () => {
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: '#FEFEFE' }}>
-            <Text>
-                This is a sign in screen
-        </Text>
+            <TextInput placeholder='Registered Email' />
+
+            <TextInput placeholder='OTP' />
+            
+            <TouchableOpacity>
+                <Text>Generate OTP</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text>Login</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     );
 }
