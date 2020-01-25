ef4d330d8dbc9be3ba7fcb50d0e59f4db4e1cfa8
codingkapoor <mailtoshivamk@gmail.com>
2020-01-26 02:30:58 +0530

    change screen to 'Feed' when push notification selected

--- src/common/listeners/index.js
+++ src/common/listeners/index.js
@@ -1,5 +1,8 @@
 import { updateActiveIntimation } from '../../store/active-intimations/actions';
+import { navigate } from '../NavigationService';
 
 export const pushNotification = notification => {
     updateActiveIntimation(notification.data);
+    if (notification.origin === 'selected')
+        navigate('Feed');
 }
