d192d92c12a461902a6488382cdc3cc44e1dad89
codingkapoor <mailtoshivamk@gmail.com>
2020-01-31 20:57:42 +0530

    details built

--- src/screens/edit/components/details/DetailsList.js
+++ src/screens/edit/components/details/DetailsList.js
@@ -6,11 +6,22 @@ class DetailsList extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { inactiveRequestDate: null };
+        this.state = { inactiveRequestDate: null, details: new Map() };
     }
 
     inactiveRequestDateSelected = inactiveRequestDate => {
-        this.setState({ inactiveRequestDate });
+        this.setState({ ...{ inactiveRequestDate } });
+
+        const res = this.props.inactiveIntimations.filter(ii => ii.requests.filter(r => r.date === inactiveRequestDate).length > 0);
+        if (res.length > 0) {
+            const id = res[0].id;
+            const firstDate = res[0].requests[0].date;
+            const lastDate = res[0].requests[res[0].requests.length - 1].date;
+            const reason = res[0].reason;
+
+            this.setState({ ...{ details: new Map([...this.state.details, ...new Map([[id, { firstDate, lastDate, reason }]])]) } });
+        }
+
         console.log(this.state);
     }
 
