2ade083dbd1d5f2a45ffef655a466a9e9c3b717e
codingkapoor <mailtoshivamk@gmail.com>
2020-02-07 06:15:54 +0530

    ref about ++ styles

--- src/screens/about/StyledComponents.js
+++ src/screens/about/StyledComponents.js
@@ -2,7 +2,26 @@ import styled from 'styled-components/native';
 
 export const Header = styled.Text`
     font-size: 20;
+    font-family: open-sans-regular;
     padding-left: 10px;
     color: #393939;
-    font-weight: bold;
+`;
+
+export const Resource = styled.Text`
+    color: blue; 
+    padding-left: 10; 
+    font-size: 18;
+    font-family: open-sans-light;
+    margin-bottom: 5;
+`;
+
+export const FooterWrapper = styled.View`
+    flex-direction: row; 
+    margin-bottom: 10; 
+    justify-content: center;
+`;
+
+export const FooterText = styled.Text`
+    font-family: open-sans-light;
+    font-size: 16;
 `;
